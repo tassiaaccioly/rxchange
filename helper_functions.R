654015513e5e155893885b613dfc39028512105b
Tassia Accioly <tassia.accioly@gmail.com>
2024-05-11 23:29:35 -0300

    changes name of function and adds documentation

--- helper_functions.R
+++ helper_functions.R
@@ -1,4 +1,8 @@
-getReversedDate <- function(date) {
+# # # # # # # # # # # # # # # # # # # # # # # # # #
+# Turns dates in any format into DD-MM-YYYY dates #
+# # # # # # # # # # # # # # # # # # # # # # # # # #
+
+getNormalizedDate <- function(date) {
   
   day <- ""
   month <- ""
@@ -19,6 +23,11 @@ getReversedDate <- function(date) {
   
   return(finalDate)
 }
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Calls the Olinda (BACEN) API with arguments and returns the one year  #
+#             historical series for the specified currency              #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 getHistoricalSeries <- function(currency, initialDate, finalDate) {
   url <- stringr::str_interp("https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@moeda='${currency}'&@dataInicial='${initialDate}'&@dataFinalCotacao='${finalDate}'&$top=360&$format=json")
