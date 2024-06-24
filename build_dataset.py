b5ce0603d9e64d3ec199d6a84976f188c9fd939d
Tassia Accioly <tassia.accioly@gmail.com>
2024-06-23 21:27:08 -0300

    improve data scraping

--- build_dataset.py
+++ build_dataset.py
@@ -1,13 +1,19 @@
 from selenium import webdriver
+from __future__ import print_function, unicode_literals
+from pprint import pprint
+from PyInquirer import prompt, Separator
 import bs4
 import time
 import re
+import openpyxl
 
 driver = webdriver.Chrome()
 
 todaysDate = str(r.todaysDate).split()[0]
 
-print(todaysDate)
+thisYear = str(r.thisYear)
+
+currency = 'EUR'
 
 # alternative url that will need some math to get the exchange value
 # driver.get("https://wise.com/br/pricing/send-money?sourceAmount=100&sourceCcy=EUR&targetCcy=BRL")
@@ -32,20 +38,47 @@ wiseScrape = data.select('span.text-success')
 # this line returns something like: 
 # [<span class="text-success">0.172162 EUR</span>, <span class="text-success">5.80848 BRL</span>]
 
-wiseRegex = r"\d+\.\d+ [A-Z]{3}"
-# gets everything between the > and < so, "0.172162 EUR" for example
+currencyRegex = r'EUR' if currency == 'EUR' else r'USD'
 
 wiseQuotas = []
 
+wiseQuotas.append(todaysDate)
+
 for i in range(len(wiseScrape)):
   wiseScrape[i] = str(wiseScrape[i])
-  print(wiseScrape[i])
-  # this line searches for the value + exchange currency symbol in the wiseScrape
-  # returns, and splits them into two ["value", "currency symbol"]
-  tempValue = re.search(wiseRegex, wiseScrape[i]).group().split()
-  wiseQuotas.extend(tempValue)
+  # this part saves the currency from the first element on the list (X.XXX EUR/USD)
+  tempCurrency = re.search(currencyRegex, wiseScrape[i])
+  if (tempCurrency):
+    tempCurrency = tempCurrency.group()
+    print(tempCurrency)
+    wiseQuotas.append(tempCurrency)
+  # this part saves the value from the second element of the list (X.XXX BRL)
+  tempValue = re.search(r'\d+\.\d+ BRL', wiseScrape[i])
+  if (tempValue):
+    tempValue = tempValue.group().split()[0]
+    print(tempValue)
+    wiseQuotas.append(tempValue)
 
+# Adds data to the exchange_historic_series excel spreadsheet
 
+histSeries = openpyxl.load_workbook('exchanges_historic_series.xlsx')
 
+# defines the sheet that should be used based on the year.
 
+try:
+  sheet = histSeries[thisYear]
+except:
+  print(f'Worksheet {thisYear} does not exist')
+  print(f'Creating worksheet {thisYear}...')
+  
+  print('Determining the sheet index...')
+  sheetToUse = histSeries.sheetnames
+  sheetToUse = sheetToUse[len(sheetToUse) - 1]
+  sheet = histSeries[sheetToUse]
+  
+  histSeries.create_sheet(index=len(sheetToUse)-1, title=thisYear)
+  
+  sheet = histSeries[thisYear]
 
+sheet.get_highest_column()
+sheet['A1'].value
