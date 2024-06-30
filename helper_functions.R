695d23e8f0b603541def683ecc52f7c0800b54be
Tassia Accioly <tassia.accioly@gmail.com>
2024-06-29 22:23:52 -0300

    adds new helper function to R script

--- helper_functions.R
+++ helper_functions.R
@@ -1,25 +1,25 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Turns dates in any format into DD-MM-YYYY dates #
+#             or MM-DD-YYYY dates                 #
 # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 getNormalizedDate <- function(date, style) {
-  
   day <- ""
   month <- ""
   
-  if(day(date) <= 9) {
+  if (day(date) <= 9) {
     day <- stringr::str_interp("0${day(date)}")
   } else {
     day <- day(date)
   }
   
-  if(month(date) <= 9) {
+  if (month(date) <= 9) {
     month <- stringr::str_interp("0${month(date)}")
   } else {
     month <- month(date)
   }
   
-  if(style == "us") {
+  if (style == "us") {
     return(stringr::str_interp("${month}-${day}-${year(date)}"))
   } else {
     return(stringr::str_interp("${day}-${month}-${year(date)}"))
@@ -32,15 +32,35 @@ getNormalizedDate <- function(date, style) {
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 getHistoricalSeries <- function(currency, initialDate, finalDate) {
-  url <- stringr::str_interp("https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@moeda='${currency}'&@dataInicial='${initialDate}'&@dataFinalCotacao='${finalDate}'&$top=360&$format=json")
+  url <- stringr::str_interp(
+    "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@moeda='${currency}'&@dataInicial='${initialDate}'&@dataFinalCotacao='${finalDate}'&$top=9999&$format=json"
+  )
   
   res <- GET(url)
   
-  if(res$status_code == 200) {
+  if (res$status_code == 200) {
     return(fromJSON(rawToChar(res$content))$value)
   } else {
     print("SOMETHING WENT WRONG")
     return(res$status_code)
   }
   
+}
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#  Returns the correct foreign exchange for the given exchange code #
+#     & the correct exchange code for the given foreign exchange    #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+exchangeMatch <- tibble(code = c("EUR", "USD"),
+                        coin = c("euro", "dollar"))
+
+fromCodeToCoin <- function(code) {
+  index <- match(code, exchangeMatch$code)
+  return(exchangeMatch$coin[index])
+}
+
+fromCoinToCode <- function(coin) {
+  index <- match(coin, exchangeMatch$coin)
+  return(exchangeMatch$code[index])
 }
