a2c2ff8fa2c7f72efa2f0e126dcf89e4218f2d8e
Tassia Accioly <tassia.accioly@gmail.com>
2024-09-25 02:24:22 -0300

    adds the 3months dataset

--- scripts/import_dataset.py
+++ scripts/import_dataset.py
@@ -74,6 +74,43 @@ df_eur_5year = df_eur_5year.reindex(["dateTime", "eur", "logEUR"], axis=1)
 # save to csv
 df_eur_5year.to_csv("./datasets/wrangled/df_eur_5year.csv", index=False)
 
+# In[1.4]: Import usd 3 months
+
+with open('./datasets/wise_eur_3months.json') as file:
+    data = json.load(file)
+
+df_eur_3months = pd.json_normalize(data)
+
+df_eur_3months.drop(['source', 'target'], axis=1, inplace=True)
+
+# rename columns
+df_eur_3months.columns = ["eur", "dateTime"]
+
+# transform dateTime into strings to not upset pandas
+df_eur_3months['dateTime'] = df_eur_3months['dateTime'].astype(str)
+
+# iterate over dateTime timestamps and turn them into actual dates.
+for index, row in df_eur_3months.loc[:,['dateTime']].iterrows():
+    df_eur_3months.at[index, 'dateTime'] = datetime.fromtimestamp(pd.to_numeric(row['dateTime']) / 1000, tz=utc_tz)
+
+# transform dateTime back into date type
+df_eur_3months['dateTime'] = pd.to_datetime(df_eur_3months['dateTime'])
+
+# add a log of "usd" column
+df_eur_3months['logEUR'] = np.log(df_eur_3months['eur'])
+
+# add a differenced column
+df_eur_3months["diff"] = df_eur_3months["logEUR"].diff()
+
+# remove NA columns
+df_eur_3months = df_eur_3months.dropna()
+
+#re-order columns
+df_eur_3months = df_eur_3months.reindex(["dateTime", "eur", "logEUR", "diff"], axis=1)
+
+# save to csv
+df_eur_3months.to_csv("./datasets/arima_ready/eur_arima_3months.csv", index=False)
+
 # In[1.3]: Import usd 1year:
 
 with open('./datasets/wise_usd_1year.json') as file:
@@ -128,7 +165,7 @@ for index, row in df_usd_5year.loc[:,['dateTime']].iterrows():
 # transform dateTime back into date type
 df_usd_5year['dateTime'] = pd.to_datetime(df_usd_5year['dateTime'])
 
-# add a log of "eur" column
+# add a log of "usd" column
 df_usd_5year['logUSD'] = np.log(df_usd_5year['usd'])
 
 #re-order columns
@@ -136,3 +173,40 @@ df_usd_5year = df_usd_5year.reindex(["dateTime", "usd", "logUSD"], axis=1)
 
 # save to csv
 df_usd_5year.to_csv("./datasets/wrangled/df_usd_5year.csv", index=False)
+
+# In[1.4]: Import usd 3 months
+
+with open('./datasets/wise_usd_3months.json') as file:
+    data = json.load(file)
+
+df_usd_3months = pd.json_normalize(data)
+
+df_usd_3months.drop(['source', 'target'], axis=1, inplace=True)
+
+# rename columns
+df_usd_3months.columns = ["usd", "dateTime"]
+
+# transform dateTime into strings to not upset pandas
+df_usd_3months['dateTime'] = df_usd_3months['dateTime'].astype(str)
+
+# iterate over dateTime timestamps and turn them into actual dates.
+for index, row in df_usd_3months.loc[:,['dateTime']].iterrows():
+    df_usd_3months.at[index, 'dateTime'] = datetime.fromtimestamp(pd.to_numeric(row['dateTime']) / 1000, tz=utc_tz)
+
+# transform dateTime back into date type
+df_usd_3months['dateTime'] = pd.to_datetime(df_usd_3months['dateTime'])
+
+# add a log of "usd" column
+df_usd_3months['logUSD'] = np.log(df_usd_3months['usd'])
+
+# add a differenced column
+df_usd_3months["diff"] = df_usd_3months["logUSD"].diff()
+
+# remove NA columns
+df_usd_3months = df_usd_3months.dropna()
+
+#re-order columns
+df_usd_3months = df_usd_3months.reindex(["dateTime", "usd", "logUSD", "diff"], axis=1)
+
+# save to csv
+df_usd_3months.to_csv("./datasets/arima_ready/usd_arima_3months.csv", index=False)
