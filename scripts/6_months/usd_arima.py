84289d71cf87058bf02fb3298684e4695f040c7a
Tassia Accioly <tassia.accioly@gmail.com>
2024-10-14 04:29:06 -0300

    remove unnused arima model from code

--- scripts/6_months/usd_arima.py
+++ scripts/6_months/usd_arima.py
@@ -75,9 +75,6 @@ arima_6months_test_010 = ARIMA(y, exog=None, order=(0,1,0)).fit()
 
 arima_6months_test_010.summary()
 
-arima_6months_test_011 = ARIMA(y, exog=None, order=(0,1,1)).fit()
-
-arima_6months_test_011.summary()
 
 # SARIMAX(0,1,0) LL 443 AIC -885 BIC -882
 # SARIMAX(2,1,0) && (1,1,1) LL 444, AIC -883, BIC -874
@@ -120,20 +117,16 @@ fig = arima_6months_test_010.plot_diagnostics(figsize=(15,10), lags=30)
 plt.suptitle("Diagnóstico dos Resíduos - ARIMA (0,1,0)", fontsize="18")
 plt.show()
 
-plt.figure(dpi=600)
-fig = arima_6months_test_011.plot_diagnostics(figsize=(15,10), lags=30)
-plt.suptitle("Diagnóstico dos Resíduos - ARIMA (0,1,1)", fontsize="18")
-plt.show()
 
 # In[1.1]: Saving and plotting the residuals for inspection
 
-usd_train_arima["erros010"] = arima_6months_test_010.resid
+usd_train_arima["erros"] = arima_6months_test_010.resid
 
-usd_train_arima["fitted010"] = arima_6months_test_010.fittedvalues
+usd_train_arima["fitted"] = arima_6months_test_010.fittedvalues
 
-erros_010 = usd_train_arima["erros010"][1:]
+erros = usd_train_arima["erros"][1:]
 
-het_arch(erros_010)
+het_arch(erros)
 
 """
 Engle's Test for Autoregressive Conditional Heteroskedasticity (ARCH)'
@@ -150,7 +143,7 @@ the residuals are homoskedastic, meaning they have constant variance meaning the
 model was well fitted.
 """
 
-erros_010.describe()
+erros.describe()
 
 """
 count    129.000000
@@ -164,53 +157,21 @@ max        0.019646
 Name: erros, dtype: float64
 """
 
-usd_train_arima["erros011"] = arima_6months_test_011.resid
-
-usd_train_arima["fitted011"] = arima_6months_test_011.fittedvalues
-
-new_df = usd_train_arima['log'] - usd_train_arima['erros010']
-
-erros_011 = usd_train_arima["erros011"][1:]
-
-erros_011.describe()
-
-"""
-count    129.000000
-mean       0.000551
-std        0.007718
-min       -0.021503
-25%       -0.004523
-50%        0.000273
-75%        0.004383
-max        0.019803
-Name: erros011, dtype: float64
-"""
-
 # Plot ACF and PACF of Residuals:
 
 fig, (ax1, ax2) = plt.subplots(2, figsize=(11,8), dpi=300)
-plot_acf(erros_010, ax=ax1)
-plot_pacf(erros_010, ax=ax2)
-
-
-fig, ((ax1, ax3), (ax2, ax4)) = plt.subplots(nrows=2, ncols=2, figsize=(11,7), dpi=300)
-plot_acf(erros_010, ax=ax1)
-ax1.set_title("Autocorrelação - ARIMA (0,1,0)")
-plot_pacf(erros_010, ax=ax3)
-ax3.set_title("Autocorrelação Parcial - ARIMA (0,1,0)")
-plot_acf(erros_011, ax=ax2)
-ax2.set_title("Autocorrelação - ARIMA (0,1,1)")
-plot_pacf(erros_011, ax=ax4)
-ax4.set_title("Autocorrelação Parcial - ARIMA (0,1,1)")
+plot_acf(erros, ax=ax1)
+ax1.set_title("Autocorrelação dos resíduos (ACF)")
+plot_pacf(erros, ax=ax2)
+ax2.set_title("Autocorrelação Parcial dos resíduos (PACF)")
 for ax in [ax1, ax2, ax3]:
     ax.spines['bottom'].set(linewidth=1.5, color="black")
     ax.spines['left'].set(linewidth=1.5, color="black")
 
-
 # Plot the scatter plot
 
 plt.figure(figsize=(15, 10))
-sns.scatterplot(erros_010)
+sns.scatterplot(erros)
 plt.title("Resíduos do Modelo - ARIMA(0,1,0)", fontsize="18")
 plt.xlabel("")
 plt.ylabel("")
@@ -219,7 +180,7 @@ plt.show()
 
 # Residuals tests
 
-acorr_ljungbox(erros_010**2, lags=[1, 2, 3, 4, 5, 6, 7, 14, 21, 30], return_df=True)
+acorr_ljungbox(erros**2, lags=[1, 2, 3, 4, 5, 6, 7, 14, 21, 30], return_df=True)
 
 """
       lb_stat  lb_pvalue
@@ -235,26 +196,9 @@ acorr_ljungbox(erros_010**2, lags=[1, 2, 3, 4, 5, 6, 7, 14, 21, 30], return_df=T
 30  28.975251   0.518897
 """
 
-acorr_ljungbox(erros_011, lags=[1, 2, 3, 4, 5, 6, 7, 14, 21, 30], return_df=True)
-
-"""
-      lb_stat  lb_pvalue
-1    0.001727   0.966851
-2    0.048728   0.975930
-3    0.377397   0.944871
-4    0.771110   0.942279
-5    2.397482   0.791849
-6    3.348113   0.764055
-7    3.435226   0.842038
-14  16.028888   0.311613
-21  20.542689   0.487158
-30  30.083022   0.461407
-"""
-
-
 ## SHAPIRO-WILK TEST
 
-shapiro(erros_010)
+shapiro(erros)
 
 # Shapiro Result
 #            statistic |             pvalue
@@ -264,19 +208,9 @@ shapiro(erros_010)
 # are normally distribuited
 # Shapiro-Wilk shows that the residuals are normally distributed.
 
-shapiro(erros_011)
-
-# Shapiro Result
-#            statistic |              pvalue
-#   0.9899967777616905 | 0.47925330565350244
-
-# p-value > 0.05 so we DO NOT REJECT the null hypothesis, so we can confirm residuals
-# are normally distribuited
-# Shapiro-Wilk shows that the residuals are normally distributed.
-
 ## JARQUE-BERA TEST
 
-jarque_bera(erros_010)
+jarque_bera(erros)
 
 # Significance Result
 #             statistic |              pvalue
@@ -286,20 +220,11 @@ jarque_bera(erros_010)
 # FOLLOW A NORMAL DISTRIBUTION
 # The Jarque-Bera test comproves the residuals are NORMALLY DISTRIBUTED
 
-jarque_bera(erros_011)
-
-# Significance Result
-#             statistic |              pvalue
-#   0.11627146453133369 |  0.9435218723835918
-
-# The p-value > 0.05 so we DO NOT REJECT the null hypothesis and confirm residuals DO
-# FOLLOW A NORMAL DISTRIBUTION
-# The Jarque-Bera test comproves the residuals are NORMALLY DISTRIBUTED
 
 sns.set_palette("viridis")
 plt.figure(figsize=(15, 10), dpi=600)
 sns.lineplot(usd_train_arima["log"], label="Valores reais (log)", linewidth=3)
-sns.lineplot(usd_train_arima["fitted010"][1:], label="Fitted Values - 010", linewidth=3)
+sns.lineplot(usd_train_arima["fitted"][1:], label="Fitted Values - 010", linewidth=3)
 # plt.title("Valores reais x Fitted Values - USD - ARIMA(0,1,0)", fontsize="18")
 plt.yticks(np.arange(round(usd_train_arima["log"].min() - 0.01, 2), round(usd_train_arima["log"].max() + 0.01, 2), 0.01), fontsize="14")
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d, %b'))
@@ -314,14 +239,12 @@ plt.show()
 
 # Revert Fitted Values:
 
-usd_train_arima["invfit010"] = invert_series(usd_train_arima["fitted010"])
-usd_train_arima["invfit011"] = invert_series(usd_train_arima["fitted011"])
+usd_train_arima["invfit"] = invert_series(usd_train_arima["fitted"])
 
 sns.set_palette("viridis")
 plt.figure(figsize=(15, 10), dpi=300)
 sns.lineplot(usd_train_arima["usd"][1:], label="Valores reais", linewidth=3)
-sns.lineplot(usd_train_arima["invfit010"][1:], label="Fitted Values", linewidth=3)
-sns.lineplot(usd_train_arima["invfit011"][1:], label="Fitted Values", linewidth=3)
+sns.lineplot(usd_train_arima["invfit"][1:], label="Fitted Values", linewidth=3)
 # plt.title("Valores reais x Fitted Values - USD - ARIMA(0,1,0)", fontsize="18")
 plt.yticks(np.arange(round(usd_train_arima["usd"].min() - 0.1, 2), round(usd_train_arima["usd"].max() + 0.1, 2), 0.1), fontsize="14")
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d, %b'))
@@ -343,33 +266,7 @@ usd_test_arima = pd.DataFrame(df_usd_arima_6months_test.drop(["date"], axis=1).v
 
 # In[2.1]: Get predictted values
 
-usd_forecast_arima = pd.concat([usd_test_arima, invert_series(arima_6months_test_010.get_forecast(steps=5).summary_frame())], axis=1).reindex(usd_test_arima.index)
-
-usd_forecast_arima = usd_forecast_arima.rename(columns={"mean": "mean010", "mean_se": "mean_se010", "mean_ci_lower": "mean_ci_lower010", "mean_ci_upper": "mean_ci_upper010"})
-
-usd_forecast_arima = pd.concat([usd_forecast_arima, invert_series(arima_6months_test_011.get_forecast(steps=5).summary_frame())], axis=1)
-
-usd_forecast_arima = usd_forecast_arima.rename(columns={"mean": "mean011", "mean_se": "mean_se011", "mean_ci_lower": "mean_ci_lower011", "mean_ci_upper": "mean_ci_upper011"})
-
-usd_arima_forecast = arima_6months_test.get_forecast(steps=5).summary_frame()
-
-
-sns.set_palette("viridis")
-fig, ax = plt.subplots(figsize=(15, 10), dpi=300)
-sns.lineplot(usd_test_arima["usd"], label="Valores de Teste", linewidth=3)
-sns.lineplot(usd_forecast_arima["mean010"], label="Valores preditos - 010", linewidth=3)
-sns.lineplot(usd_forecast_arima["mean011"], label="Valores preditos - 011", linewidth=3)
-# plt.title("Valores reais x Fitted Values", fontsize="18")
-plt.yticks(np.arange(round(usd_forecast_arima["usd"].min() - 0.1, 2), round(usd_forecast_arima["usd"].max() + 0.1, 2), 0.1), fontsize="14")
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d, %b'))
-plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=1))
-plt.gca().xaxis.set_tick_params(rotation = -30)
-plt.xticks(fontsize="14")
-plt.xlabel("Data", fontsize="18")
-plt.ylabel("Câmbio USD ↔ BRL", fontsize="18")
-plt.legend(fontsize=18, loc="lower right", bbox_to_anchor=(0.99, 0.05, 0, 0))
-plt.show()
-
+usd_forecast_arima = pd.concat([usd_test_arima, invert_series(arima_6months_test.get_forecast(steps=5).summary_frame())], axis=1).reindex(usd_test_arima.index)
 
 sns.set_palette("viridis")
 fig, ax = plt.subplots(figsize=(15, 10), dpi=300)
